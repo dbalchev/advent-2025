88f97557281da51dd6bc4b2bf51fe62e2f985c1a
Daniel Balchev <dbalchev@gmail.com>
2025-11-30 19:33:43 +0200

    Make day 00

--- aoc-library/context.go
+++ aoc-library/context.go
@@ -2,16 +2,11 @@ package aoclibrary
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/fatih/color"
 )
 
 type Context struct {
-}
-
-func (context *Context) Fscanf(format string, a ...any) (n int, err error) {
-	return fmt.Fscanf(os.Stdin, format, a...)
 }
 
 func (context *Context) Eprintf(format string, a ...any) {
--- solutions/day00.go
+++ solutions/day00.go
@@ -1,6 +1,12 @@
 package solutions
 
-import aoclibrary "github.com/dbalchev/advent-2025/aoc-library"
+import (
+	"fmt"
+	"io"
+	"slices"
+
+	aoclibrary "github.com/dbalchev/advent-2025/aoc-library"
+)
 
 func init() {
 	aoclibrary.Register(0, &Day00{})
@@ -8,7 +14,62 @@ func init() {
 
 type Day00 struct{}
 
+func iabs(x int) int {
+	if x >= 0 {
+		return x
+	}
+	return -x
+}
+
 func (*Day00) Solve(context *aoclibrary.Context) error {
-	context.Solution(1, "foo")
+	lefts := make([]int, 0)
+	rights := make([]int, 0)
+	for {
+		left := -1
+		right := -1
+		n, err := fmt.Scanf("%d %d\n", &left, &right)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n != 2 {
+			break
+		}
+		lefts = append(lefts, left)
+		rights = append(rights, right)
+
+	}
+	fmt.Printf("lefts = %v; rights = %v\n", lefts, rights)
+	context.Solution(1, solve1(slices.Clone(lefts), slices.Clone(rights)))
+
+	sum := solve2(rights, lefts)
+	context.Solution(2, sum)
+
 	return nil
+}
+
+func solve1(lefts []int, rights []int) int {
+	slices.Sort(lefts)
+	slices.Sort(rights)
+	sum := 0
+	for i := range lefts {
+		sum += int(iabs(lefts[i] - rights[i]))
+	}
+	return sum
+}
+
+func solve2(rights []int, lefts []int) int {
+	rightCounts := make(map[int]int)
+	for _, x := range rights {
+		c, exists := rightCounts[x]
+		if exists {
+			rightCounts[x] = c + 1
+		} else {
+			rightCounts[x] = 1
+		}
+	}
+	sum := 0
+	for _, x := range lefts {
+		sum += rightCounts[x] * x
+	}
+	return sum
 }
