c63b87214ca5ef836dd379090d13e658945c1112
Daniel Balchev <dbalchev@gmail.com>
2025-12-06 12:42:12 +0200

    Day06B

--- solutions/day06.go
+++ solutions/day06.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"math/big"
 	"os"
+	"unicode"
 
 	aoclibrary "github.com/dbalchev/advent-2025/aoc-library"
 )
@@ -18,12 +19,101 @@ func init() {
 type day06 struct{}
 
 // A - 9:33-9:47
+// B - 9:51-10:09, 12:00
 func (*day06) Solve(context *aoclibrary.Context) error {
 	input, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		return err
 	}
 	lines := bytes.Split(bytes.Trim(input, "\n"), []byte("\n"))
+	numbersA, opsA, err := parse06A(lines)
+	if err != nil {
+		return err
+	}
+	resultA, err := processMath06(opsA, numbersA)
+	if err != nil {
+		return err
+	}
+	context.Solution("A", resultA)
+	numbersB, opsB, err := parse06B(lines)
+	if err != nil {
+		return err
+	}
+	resultB, err := processMath06(opsB, numbersB)
+	if err != nil {
+		return err
+	}
+	context.Solution("B", resultB)
+	return nil
+}
+
+func transpose(m [][]*big.Int) [][]*big.Int {
+	r := make([][]*big.Int, 0)
+	nElements := len(m[0])
+	for i := range m[0] {
+		newRow := make([]*big.Int, 0)
+		for _, oldRow := range m {
+			if len(oldRow) != nElements {
+				panic("not rectangular matrix")
+			}
+			newRow = append(newRow, oldRow[i])
+		}
+		r = append(r, newRow)
+	}
+	return r
+}
+
+func parse06B(lines [][]byte) ([][]*big.Int, []string, error) {
+	opColumns := make([]int, 0)
+	lastLine := lines[len(lines)-1]
+	for i, c := range lastLine {
+		if !unicode.IsSpace(rune(c)) {
+			opColumns = append(opColumns, i)
+		}
+	}
+	opColumns = append(opColumns, len(lastLine)+1)
+	slog.Debug("opColumns", "", opColumns)
+	for opColIndex, opColumn := range opColumns[1 : len(opColumns)-1] {
+		for lineIndex, line := range lines {
+			if !unicode.IsSpace(rune(line[opColumn-1])) {
+				return nil, nil, fmt.Errorf("unexpected non-space on %d:%d", lineIndex, opColIndex)
+			}
+
+		}
+	}
+	ops := make([]string, 0)
+	numbers := make([][]*big.Int, 0)
+
+	numersLens := make([]int, 0)
+	for i, columnStart := range opColumns[:len(opColumns)-1] {
+		columnEnd := opColumns[i+1] - 1
+		ops = append(ops, string(lastLine[columnStart:columnStart+1]))
+		colNumbers := make([]*big.Int, 0)
+		for j := columnEnd - 1; j >= columnStart; j -= 1 {
+			numStr := make([]byte, 0)
+			for _, line := range lines[0 : len(lines)-1] {
+				c := line[j]
+				numStr = append(numStr, c)
+			}
+			if len(numStr) == 0 {
+				return nil, nil, fmt.Errorf("unexpected empty number")
+			}
+			numStr = bytes.Trim(numStr, " ")
+			number := big.Int{}
+			_, ok := number.SetString(string(numStr), 10)
+			if !ok {
+				return nil, nil, fmt.Errorf("cannot parse %q", string(numStr))
+			}
+			colNumbers = append(colNumbers, &number)
+		}
+		numbers = append(numbers, colNumbers)
+		numersLens = append(numersLens, len(colNumbers))
+	}
+	// slog.Debug("numbersLen", "numbersLen", numersLens)
+	return numbers, ops, nil
+}
+
+func parse06A(lines [][]byte) ([][]*big.Int, []string, error) {
 	numbers := make([][]*big.Int, 0)
 	for _, line := range lines[:len(lines)-1] {
 		lineNumbers := make([]*big.Int, 0)
@@ -31,7 +121,7 @@ func (*day06) Solve(context *aoclibrary.Context) error {
 			number := big.Int{}
 			_, ok := number.SetString(string(field), 10)
 			if !ok {
-				return fmt.Errorf("Cannot parse %q", string(field))
+				return nil, nil, fmt.Errorf("cannot parse %q", string(field))
 			}
 			lineNumbers = append(lineNumbers, &number)
 		}
@@ -41,6 +131,12 @@ func (*day06) Solve(context *aoclibrary.Context) error {
 	for _, field := range bytes.Fields(lines[len(lines)-1]) {
 		ops = append(ops, string(field))
 	}
+	numbers = transpose(numbers)
+
+	return numbers, ops, nil
+}
+
+func processMath06(ops []string, numbers [][]*big.Int) (*big.Int, error) {
 	slog.Debug("ops", "ops", ops)
 	slog.Debug("numbers", "numbers", numbers)
 	accum := big.NewInt(0)
@@ -59,14 +155,13 @@ func (*day06) Solve(context *aoclibrary.Context) error {
 				opAccum = opAccum.Mul(opAccum, x)
 			}
 		default:
-			return fmt.Errorf("cannot parse op %q", op)
+			return nil, fmt.Errorf("cannot parse op %q", op)
 		}
-		for _, line := range numbers {
-			update(line[i])
+		for _, x := range numbers[i] {
+			update(x)
 		}
 		slog.Debug("op result", "result", opAccum)
 		accum = accum.Add(accum, opAccum)
 	}
-	context.Solution("A", accum)
-	return nil
+	return accum, nil
 }
