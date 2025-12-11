e62aa6b2ba36eb57b0acd04283fce2d0f7276db4
Daniel Balchev <dbalchev@gmail.com>
2025-12-11 21:31:43 +0200

    Day11B

--- solutions/day11.go
+++ solutions/day11.go
@@ -16,6 +16,54 @@ func init() {
 
 type day11 struct{}
 
+func solve11(
+	children, parents map[string][]string,
+	allNodes map[string]bool,
+	startNode, endNode string,
+	intermediateNodes []string,
+) (int, error) {
+	type state struct {
+		node        string
+		imNodesMask int
+	}
+	pathCount := make(map[state]int)
+	pathCount[state{node: endNode, imNodesMask: 0}] = 1
+	stack := []state{{node: endNode, imNodesMask: 0}}
+	nRemainingChildren := make(map[string]int)
+	for node := range allNodes {
+		nRemainingChildren[node] = len(children[node])
+	}
+	maxNodeMask := make(map[string]int)
+	for len(stack) > 0 {
+		x := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		for _, parent := range parents[x.node] {
+			newMaks := x.imNodesMask
+			for i, n := range intermediateNodes {
+				if n == parent {
+					newMaks |= 1 << i
+					slog.Debug("visiting", "n", n)
+				}
+			}
+			newState := state{node: parent, imNodesMask: newMaks}
+			pathCount[newState] += pathCount[x]
+			slog.Debug("adding", "newState", newState, "x", x, "pathCount[x]", pathCount[x])
+			nRemainingChildren[parent] -= 1
+			maxNodeMask[parent] = maxNodeMask[parent] | newMaks
+			if nRemainingChildren[parent] == 0 {
+				stack = append(stack, state{node: parent, imNodesMask: maxNodeMask[parent]})
+			}
+		}
+	}
+	solution, ok := pathCount[state{node: startNode, imNodesMask: (1 << len(intermediateNodes)) - 1}]
+	if !ok {
+		slog.Debug("no counts", "pathCount", pathCount, "nRemainingChildren", nRemainingChildren)
+		return 0, fmt.Errorf("no %q counts", startNode)
+	}
+	return solution, nil
+}
+
 func (*day11) Solve(context *aoclibrary.Context) error {
 	input, err := io.ReadAll(os.Stdin)
 	if err != nil {
@@ -40,28 +88,15 @@ func (*day11) Solve(context *aoclibrary.Context) error {
 		}
 	}
 	slog.Debug("input", "children", children, "parents", parents, "allNodes", allNodes)
-	pathCount := make(map[string]int)
-	pathCount["out"] = 1
-	stack := []string{"out"}
-	nRemainingChildren := make(map[string]int)
-	for node := range allNodes {
-		nRemainingChildren[node] = len(children[node])
-	}
-	for len(stack) > 0 {
-		x := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-		for _, parent := range parents[x] {
-			pathCount[parent] += pathCount[x]
-			nRemainingChildren[parent] -= 1
-			if nRemainingChildren[parent] == 0 {
-				stack = append(stack, parent)
-			}
-		}
-	}
-	solutionA, ok := pathCount["you"]
-	if !ok {
-		return fmt.Errorf("no you counts")
+	solutionA, err := solve11(children, parents, allNodes, "you", "out", []string{})
+	if err != nil {
+		return err
 	}
 	context.Solution("A", solutionA)
+	solutionB, err := solve11(children, parents, allNodes, "svr", "out", []string{"fft", "dac"})
+	if err != nil {
+		return err
+	}
+	context.Solution("B", solutionB)
 	return nil
 }
